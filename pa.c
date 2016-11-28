934b7738bf085f3401bf3ae07825c008b8fb0132
lyon2412 <lyon_24_12@hotmail.com>
2016-11-28 17:40:52 -0500

    Add files via upload

--- pa.c
+++ pa.c
@@ -3,37 +3,52 @@
 #define N 10
 
 int i=0, j=0;
-void ingresar(char *c);
-//void ordenar();
+void ingresar(char *x);
+void ordenar(char *x);
 
 void main(){
-char *c, ch;
+char *c=NULL;
 c = (char*)malloc(N*sizeof(char));
 	ingresar(c);
 	printf("%d\n",i);
 	printf("%s\n",c);
 	c = (char*) realloc(c,(i+1)*sizeof(char));
-
+	ordenar(c);
+	printf("%s\n",c);
 
 
 }
-void ingresar(char *c){
+void ingresar(char *x){
 
 	char ch;
 	printf("ingrese numeros naturales:\n");
 	while((ch=getchar())!='-'){
 	if(j>9){
-		//c = (char *)realloc(c,(N+i)*sizeof(char)); 
+		x = (char *)realloc(x,(N+i)*sizeof(char)); 
 		j=0;
 	}
-	*(c+i) = ch;
+	*(x+i) = ch;
 	i++;   
 	j++;
 	}	
 }
 
-//void ordenar(){	
-	//k=0;
-	//while(k<i){
+void ordenar(char *x){	
+	int k=1, aux, l;
+	for(k=1;k<i;k++){
+		for(l=k;l<i;l++){
+			if(*(x+l)<*(x+k-1)){
+				aux=*(x+l);
+				*(x+l)=*(x+k-1);
+				*(x+k-1)=aux;
+			}
+		}
+	}		
+}
 		
+	
+	
+	
+	
+	
 	
